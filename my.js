27afda4c32055c8fcab303be5505187b5cf0c70e
DanyaHomenko <74784472+DanyaHomenko@users.noreply.github.com>
2021-02-24 17:45:03 +0200

    Add files via upload

---
+++ my.js
@@ -0,0 +1,19 @@
+let newNik = prompt("Please enter nickname:");
+    alert(`Your nickname is ${newNik}`);
+    let newpass = prompt("Please enter password:");
+    alert(`Your password is ${newpass}`);
+
+    let Nik = prompt("Please enter nickname:");
+if (Nik == newNik){
+    alert("I found this nickname");
+} else {
+    alert("I not found this nickname");
+}
+
+let password = prompt("Please enter password");
+if(password = newpass){
+    alert("password is true");
+    alert("Welcome");
+} else {
+    alert("password is false");
+}
